e59c2f32906054e803ee2c639420a40d3848dda3
Sigfrid Lundberg <sigfrid@sigfrid-lundberg.se>
2021-03-23 22:04:36 +0100

    both source and products

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+soelim from_different_angles.ms |groff -U -p -Tps -tk -Kutf8 -M/home/sigge/documents/tmac -ma4 -P-pa4 -ms -mpdfmark > from_different_angles.ps
+
+ps2pdf from_different_angles.ps
+
+gs -r300  -sDEVICE=jpeg -sOutputFile=from_different_angles_%d.jpeg - < from_different_angles.ps
+
+
